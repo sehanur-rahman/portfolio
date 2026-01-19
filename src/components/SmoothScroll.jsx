5266acbdd121feb6ecdb77b4ba5002398d13f0b4
sehanur24 <sehanurrahmansiam@gmail.com>
2026-01-20 03:25:46 +0600

    Add something

--- src/components/SmoothScroll.jsx
+++ src/components/SmoothScroll.jsx
@@ -10,7 +10,7 @@ const SmoothScroll = ({ children, isLoading }) => {
       duration: 1.5,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       orientation: "vertical",
-      gestureOrientation: "vertical",
+      gestureOrientation: "vertical", 
       smoothWheel: true,
       wheelMultiplier: 1,
       touchMultiplier: 2,
